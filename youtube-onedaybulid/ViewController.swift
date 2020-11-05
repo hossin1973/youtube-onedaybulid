089fb408a97cc81818c66ff6d51f14a2b38f223b
Hossin <saldanaryana@macbook-pro.speedport_w_724v_typ_a_05011603_06_003>
2020-11-05 08:16:17 +0100

    initial xcode project creation

---
+++ youtube-onedaybulid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybulid
+//
+//  Created by Hossin on 30.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
